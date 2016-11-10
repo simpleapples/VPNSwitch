537dadc710608ea377c61ac15cb396874ecc71d6
simpleapples <zangzhiya@gmail.com>
2016-11-10 17:57:32 +0800

    feat: 添加填写OnDemand rules的界面

---
+++ VPNSwitch/Controllers/OnDemandViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OnDemandViewController.swift
+//  VPNSwitch
+//
+//  Created by Zzy on 10/11/2016.
+//  Copyright © 2016 Zzy. All rights reserved.
+//
+
+import UIKit
+
+class OnDemandViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
--- VPNSwitch/Controllers/SettingViewController.swift
+++ VPNSwitch/Controllers/SettingViewController.swift
@@ -21,7 +21,9 @@ class SettingViewController: UITableViewController {
 extension SettingViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 1 && indexPath.row == 0 {
+        if indexPath.section == 0 && indexPath.row == 1 {
+            self.performSegue(withIdentifier: "SettingToOnDemandSegue", sender: nil)
+        } else if indexPath.section == 1 && indexPath.row == 0 {
             let popup = PopupDialog(title: "删除「VPN开关」创建所有配置", message: "点击「删除」可以删除「VPN开关」在iOS设置中创建的所有VPN配置文件，其它App和用户自己创建的VPN不受影响", image: nil, buttonAlignment: .horizontal, transitionStyle: .zoomIn, gestureDismissal: true, completion: nil)
             let destructiveButton = DestructiveButton(title: "删除") {
                 VPNManager.sharedManager.deleteVPNConfigurations()
@@ -30,11 +32,17 @@ extension SettingViewController {
             }
             popup.addButtons([destructiveButton, cancelButton])
             self.present(popup, animated: true, completion: nil)
-        } else if indexPath.section == 2 && indexPath.row == 0 {
-            let path = Bundle.main.path(forResource: "Pods-VPNSwitch-acknowledgements", ofType: "plist")
-            let acknowListViewController = AcknowListViewController(acknowledgementsPlistPath: path)
-            if let navigationController = self.navigationController {
-                navigationController.show(acknowListViewController, sender: nil)
+        } else if indexPath.section == 2 {
+            if indexPath.row == 0 {
+                let path = Bundle.main.path(forResource: "Pods-VPNSwitch-acknowledgements", ofType: "plist")
+                let acknowListViewController = AcknowListViewController(acknowledgementsPlistPath: path)
+                if let navigationController = self.navigationController {
+                    navigationController.show(acknowListViewController, sender: nil)
+                }
+            } else if indexPath.row == 1 {
+                let appID = "1111804359";
+                let url = URL(string: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=\(appID)")
+                UIApplication.shared.open(url!, options: [:], completionHandler: nil)
             }
         }
     }
